db817a68e818fc8ce8f22e283a2251f0c3e1a602
killje <patrick.beuks@home.nl>
2014-08-21 16:08:23 +0200

    added test case

--- MinecraftTestingGrounds/src/main/java/me/killje/minecrafttestinggrounds/Main.java
+++ MinecraftTestingGrounds/src/main/java/me/killje/minecrafttestinggrounds/Main.java
@@ -1,16 +1,24 @@
 package me.killje.minecrafttestinggrounds;
 
+import java.util.ArrayList;
+import net.minecraft.server.v1_7_R3.DamageSource;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Firework;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.projectiles.ProjectileSource;
 import org.bukkit.util.Vector;
 
 /**
@@ -33,15 +41,25 @@ public class Main extends JavaPlugin implements Listener {
                 double startShift = 2;
                 Vector shootShiftVector = new Vector(directionVector.getX() * startShift, directionVector.getY() * startShift + 1.5, directionVector.getZ() * startShift);
                 shootLocation = shootLocation.add(shootShiftVector.getX(), shootShiftVector.getY(), shootShiftVector.getZ());
-                
+
                 Fireball fireball = shootLocation.getWorld().spawn(shootLocation, Fireball.class);
                 fireball.setVelocity(directionVector.multiply(2.0));
                 fireball.setIsIncendiary(false);
                 fireball.setShooter(event.getPlayer());
                 fireball.setYield(0);
-                
+
                 event.setCancelled(true);
             }
+        }
+    }
+    
+    @EventHandler
+    public void onHit(EntityDamageByEntityEvent e){
+        if (!e.getEntity().getType().equals(EntityType.FIREBALL)) {
+            return;
+        }
+        if (((Fireball) e.getEntity()).getShooter() instanceof Player) {
+            //DO STUFF
         }
     }
 }
